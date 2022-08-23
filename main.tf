8bdd38435f6b4aa4afc01f6badb33577a3d36f68
ToshAshu0312 <ToshAshu0312@github.com>
2022-08-23 18:27:14 +0530

    updating terraform files

--- main.tf
+++ main.tf
@@ -1,52 +1,51 @@
 terraform {
-    required_providers {
-       aws = {
-        
-          source = "hashicorp/aws"
-          version = " ~>4.0 "
-        }
+  required_providers {
+    aws = {
+
+      source  = "hashicorp/aws"
+      version = " ~>4.0 "
     }
+  }
 }
 
+
 provider "aws" {
-    region  = "ap-south-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "myec2" {
-    ami= "ami-0cea098ed2ac54925"
-    instance_type= var.instance_type
+  ami           = "ami-076e3a557efe1aa9c"
+  instance_type = var.instance_type
+  key_name = "terraform-key"
 
-     tags = {
-        name = "Sample"
-    }
+  tags = {
+    name = "Sample"
+  }
 
-connection{
-    type = "ssh"
-    user= "ec2-user"
-    password = var.root_password
-    host = self.public_ip
-}
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("./terraform-key.pem")
+    host        = aws_instance.myec2.public_ip
+  }
 
-provisioner "remote-exec" {
+  provisioner "remote-exec" {
     inline = [
-        "sudo amazon-linux-extras install -y nginx1" ,
-        "sudo systemctl start nginx"
+      "sudo amazon-linux-extras install -y nginx1",
+      "sudo systemctl start nginx"
     ]
-}
+  }
 }
 
-resource "aws_security_group" "sg" {
-  name= "terraform_security_group"
-}
 
 output "IP" {
-    value = aws_instance.myec2.public_ip
+  value = aws_instance.myec2.public_ip
 }
 
 
 
 output "ID" {
-    value = aws_instance.myec2.id
+  value = aws_instance.myec2.id
 }
 
 output "TEXT_FILE" {
