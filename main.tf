19391b19ac73e2e6eece9adc52233ef0be42a5a9
ToshAshu0312 <ToshAshu0312@github.com>
2022-08-23 14:32:31 +0530

    updating remote executor

--- main.tf
+++ main.tf
@@ -13,23 +13,43 @@ provider "aws" {
 }
 
 resource "aws_instance" "myec2" {
-    ami= "ami-0a5b0c3ca9452f414"
+    ami= "ami-0cea098ed2ac54925"
     instance_type= var.instance_type
 
-    tags = {
+     tags = {
         name = "Sample"
     }
+
+connection{
+    type = "ssh"
+    user= "ec2-user"
+    password = var.root_password
+    host = self.public_ip
+}
+
+provisioner "remote-exec" {
+    inline = [
+        "sudo amazon-linux-extras install -y nginx1" ,
+        "sudo systemctl start nginx"
+    ]
+}
+}
+
+resource "aws_security_group" "sg" {
+  name= "terraform_security_group"
 }
 
 output "IP" {
     value = aws_instance.myec2.public_ip
 }
 
+
+
 output "ID" {
     value = aws_instance.myec2.id
 }
 
 output "TEXT_FILE" {
-
   value = file("${path.module}/sample1.txt")
 }
+
