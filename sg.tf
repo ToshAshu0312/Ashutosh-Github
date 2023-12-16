ef2c905c89a4b92d6a7fc8fc53cc5e637531a39a
ToshAshu0312 <110562543+ToshAshu0312@users.noreply.github.com>
2023-12-16 23:48:52 +0530

    Create sg.tf
    Signed-off-by: ToshAshu0312 <110562543+ToshAshu0312@users.noreply.github.com>

---
+++ sg.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "demo" {
+  name        = "demo"
+  description = "creating demo security group with terraform"
+
+
+  dynamic "ingress" {
+    for_each = var.ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  dynamic "egress" {
+    for_each = var.ports
+    content {
+      from_port   = egress.value
+      to_port     = egress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+}
