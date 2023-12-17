0d49124c8a3209cbe05b3450f895a1d3ac9c6ea3
ToshAshu0312 <ashutoshupadhyay2718@gmail.com>
2023-12-17 10:28:05 +0530

    removing files

--- New_terraform/s3.tf
+++
@@ -1,8 +0,0 @@
-/* resource "aws_s3_bucket" "mys3bucket" {
-  bucket = "s3-bucket-ashutosh"
-
-  tags = {
-    Name        = "Mybuckettag"
-    environment = "Dev"
-  }
-} */
--- New_terraform/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-#instancetype = "t2.micro"
-ports = ["80", "443", "22", "8080"]
